1b869c029a4cb1772e10754346e6f8e514b2c336
JPBlancoDB <jpblancodb@gmail.com>
2019-02-09 19:36:44 +0100

    Add option for migrations, add templates

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const project = require('./src/projects');
 program
   .version(pkg.version, '-v, --version')
   .option('-p, --project [db]', 'Create new project')
+  .option('-m, --migration <name>', 'Create new migration file')
   .parse(process.argv);
 
 if (program.project) {
   project.create(program.project);
+  process.exit(0);
+}
+
+if (program.migration) {
+  console.log(program.migration);
   process.exit(0);
 }
 
